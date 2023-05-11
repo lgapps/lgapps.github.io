dfa584938a778f49ce04177fbfe02e595c54214c
“soyoung0928.choi” <soyoung0928.choi@lge.com>
2023-05-11 19:35:02 +0900

    파일 없는 경우, 에러 처리2

--- js/detail.js
+++ js/detail.js
@@ -29,46 +29,42 @@ $(document).ready(function () {
       Accept: "application/vnd.github.v3+json",
     },
     success: function (data) {
-      console.log("log. success.");
+      console.log("log. success. data.length=" + data.length);
+      const options = [];
+      const pathValue = "./" + params["country"] + "/" + params["locale"] + "/";
+      for (let i = 0; i < data.length; i++) {
+        if (data[i].type === "file") {
+          options.push(
+            `<option value="${pathValue}${data[i].name}">${data[
+              i
+            ].name.substring(0, 6)}</option>`
+          );
+          // if (data[i].name.includes("recent.html")) {
+          //   findRecentFile = data[i].name;
+          //   console.log("log. findRecentFile=" + findRecentFile);
+          // }
+          console.log("log. data[i].name=" + data[i].name);
+        }
+      }
+
+      options.sort(function (a, b) {
+        if (a < b) {
+          return 1;
+        }
+        if (a > b) {
+          return -1;
+        }
+        return 0;
+      });
+      $("#files_list").html(options.join(""));
 
       var url_default = "./default_en-US.html";
       var findRecentFile = url_default;
-      const options = [];
-      const pathValue = "./" + params["country"] + "/" + params["locale"] + "/";
-      if (data && data.length > 0) {
-        console.log("log. data.length=" + data.length);
-        for (let i = 0; i < data.length; i++) {
-          if (data[i].type === "file") {
-            options.push(
-              `<option value="${pathValue}${data[i].name}">${data[
-                i
-              ].name.substring(0, 6)}</option>`
-            );
-            // if (data[i].name.includes("recent.html")) {
-            //   findRecentFile = data[i].name;
-            //   console.log("log. findRecentFile=" + findRecentFile);
-            // }
-            console.log("log. data[i].name=" + data[i].name);
-          }
-        }
-
-        options.sort(function (a, b) {
-          if (a < b) {
-            return 1;
-          }
-          if (a > b) {
-            return -1;
-          }
-          return 0;
-        });
-        $("#files_list").html(options.join(""));
-
-        var select = document.getElementById("files_list");
-        var firstOptionValue = select.options[0].textContent;
-        if (firstOptionValue) {
-          findRecentFile = firstOptionValue + ".html";
-          console.log("log. findRecentFile=" + findRecentFile);
-        }
+      var select = document.getElementById("files_list");
+      var firstOptionValue = select.options[0].textContent;
+      if (firstOptionValue) {
+        findRecentFile = firstOptionValue + ".html";
+        console.log("log. findRecentFile=" + findRecentFile);
       }
 
       var recentUrl =
@@ -78,15 +74,13 @@ $(document).ready(function () {
         params["locale"] +
         "/" +
         findRecentFile;
-      $("#contents").load(recentUrl, function (response, status, xhr) {
-        if (status == "error") {
-          console.log("log. no file. load default page.");
-          $("#contents_default").load(url_default);
-          $("#select_files").hide();
-        } else {
-          console.log("log. file exists. status=", status);
-        }
-      });
+      $("#contents").load(recentUrl);
+    },
+    error: function (xhr, status, error) {
+      console.log("AJAX request fail:", error);
+      var url_default = "./default_en-US.html";
+      $("#contents_default").load(url_default);
+      $("#select_files").hide();
     },
   });
 });
